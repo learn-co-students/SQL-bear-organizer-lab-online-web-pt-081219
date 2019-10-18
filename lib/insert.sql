c40167da9addc13f007c0adc0c69ebfeb34e4c03
wifelette <leahsilber@gmail.com>
2019-10-17 21:34:00 -0700

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,17 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Mr. Chocolate", 11, "male", "brown", "hungry", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Rowdy", 10, "female", "brown", "hungry", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Tabitha", 6, "female", "brown", "curious", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Sergeant Brown", 11, "non-binary", "brown", "curious", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Melissa", 13, "female", "black", "grumpy", false);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Grinch", 9, "male", "black", "old", false);
+INSERT INTO bears (name, age, gender, color, temperament)
+    VALUES ("Wendy", 6, "male", "green", "hungry");
+INSERT INTO bears (age, gender, color, temperament, alive)
+    VALUES (9, "non-binary", "green", "kind", true);
+    
