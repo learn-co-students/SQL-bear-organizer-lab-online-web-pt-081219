83650ca3e42d4c74b844b4c9821fb65915d73401
CleanSolLLC <mamark90@gmail.com>
2019-10-22 10:54:02 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,19 @@
+
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (0, "Mr. Chocolate", 10, "M", "black", "subdue", true);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Rowdy", 15, "M", "brown", "aggessive", false);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (2, "Tabitha", 2, "F", "black", "subdue", true);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (3, "Sergeant Brown", 18, "M", "brown", "aggressive", false);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (4, "Melissa", 2, "F", "brown", "subdue", true);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (5, "Grinch", 4, "F", "black", "agressive", false);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (6, "Wendy", 3, "M", "brown", "subdue", false);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (7, NULL, 9, "F", "black", "agressive", true);
+
+
+-- "Rowdy"
+-- "Tabitha"
+-- "Sergeant Brown"
+-- "Melissa"
+-- "Grinch"
+-- "Wendy"
+-- unnamed (the bear that killed Tim didn't have a name; refer back to how to create a record that doesn't have one value)
